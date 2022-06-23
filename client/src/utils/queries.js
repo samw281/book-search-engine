8ebba36014118a7a4702f8e9b1f02864a8a09eac
Sam Williams <samwilliams281@gmail.com>
2022-06-23 19:49:27 -0400

    delete routes

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,100 +1,38 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
-export const QUERY_THOUGHTS = gql`
-  query thoughts($username: String) {
-    thoughts(username: $username) {
-      _id
-      thoughtText
-      createdAt
-      username
-      reactionCount
-      reactions {
-        _id
-        createdAt
-        username
-        reactionBody
-      }
-    }
-  }
-`;
-
-export const QUERY_THOUGHT = gql`
-  query thought($id: ID!) {
-    thought(_id: $id) {
-      _id
-      thoughtText
-      createdAt
-      username
-      reactionCount
-      reactions {
-        _id
-        createdAt
-        username
-        reactionBody
-      }
-    }
-  }
-`;
-
-export const QUERY_USER = gql`
-  query user($username: String!) {
-    user(username: $username) {
-      _id
-      username
-      email
-      friendCount
-      friends {
-        _id
-        username
-      }
-      thoughts {
-        _id
-        thoughtText
-        createdAt
-        reactionCount
-      }
-    }
-  }
-`;
-
-export const QUERY_ME = gql`
-  {
+export const GET_ME = gql`
+  query Me {
     me {
       _id
       username
       email
-      friendCount
-      thoughts {
-        _id
-        thoughtText
-        createdAt
-        reactionCount
-        reactions {
-          _id
-          createdAt
-          reactionBody
-          username
-        }
-      }
-      friends {
-        _id
-        username
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        image
+        link
+        title
       }
     }
   }
 `;
 
-export const QUERY_ME_BASIC = gql`
-  {
-    me {
+export const SAVE_BOOK = gql`
+  mutation SaveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
       _id
       username
-      email
-      friendCount
-      friends {
-        _id
-        username
-      }
+      bookCount
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation RemoveBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
     }
   }
 `;
